c9311bb8aaa0824ed597ce1c9c256aa51256dfb3
uana <uanavasile@gmail.com>
2019-12-09 13:56:47 +0100

    Added Easy Peasy

---
+++ src/academy/everyonecodes/java/week1/set1/exercise1/EasyPeasy.java
@@ -0,0 +1,7 @@
+package academy.everyonecodes.java.week1.set1.exercise1;
+
+public class EasyPeasy {
+    public static void main(String[] args) {
+        System.out.println("Easy peasy!");
+    }
+}
