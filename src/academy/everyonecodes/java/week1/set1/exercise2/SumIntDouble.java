971bb9b02642c43427a1abe5d0043d0458d49990
uana <uanavasile@gmail.com>
2019-12-09 15:08:39 +0100

    SumIntDouble.java

--- src/academy/everyonecodes/java/week1/set1/exercise2/SumIntDouble.java
+++ src/academy/everyonecodes/java/week1/set1/exercise2/SumIntDouble.java
@@ -10,12 +10,12 @@ public class SumIntDouble {
     }
 
     public static void main(String[] args) {
-        int nbOne = 30;
-        int nbTwo = 12;
-        int sumInt = nbOne + nbTwo;
-        double noOne = 6.1;
-        double noTwo = 6.1;
-        double sumDouble = noOne + noTwo;
+        int numberOne = 30;
+        int numberTwo = 12;
+        int sumInt = numberOne + numberTwo;
+        double numberThree = 6.1;
+        double numberFour = 6.1;
+        double sumDouble = numberThree + numberFour;
         System.out.println("The int sum is " + sumInt);
         System.out.println("The int sum is " + sumDouble);
     }
