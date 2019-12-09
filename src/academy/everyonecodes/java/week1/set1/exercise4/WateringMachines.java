2fe8231c128d6b874f88954223f79dd7dcfa71d1
uana <uanavasile@gmail.com>
2019-12-09 15:04:07 +0100

    Watering Machines

--- src/academy/everyonecodes/java/week1/set1/exercise4/WateringMachines.java
+++ src/academy/everyonecodes/java/week1/set1/exercise4/WateringMachines.java
@@ -1,13 +1,19 @@
 package academy.everyonecodes.java.week1.set1.exercise4;
 
 public class WateringMachines {
+    public static double doDiscount(double discount, int price){
+        return (discount / 100) * price;
+    }
+
     public static void main(String[] args){
         int plants = 3 + 9;
         int machine = 1;
         int plantsWateredByOneMachine = 4;
-        int numberOfWateringMachinesToBuy = (plants / plantsWateredByOneMachine) - 1;
+        int numberOfWateringMachinesToBuy = (plants / plantsWateredByOneMachine) - machine;
         int pricePerMachine = 20;
-        int finalCost = numberOfWateringMachinesToBuy * (pricePerMachine - (pricePerMachine / 100) * 20);
+        int discount = 20;
+        double discountValue = doDiscount(discount, pricePerMachine);
+        double finalCost = numberOfWateringMachinesToBuy * (pricePerMachine - discountValue);
         System.out.println("The final cost is " + finalCost);
     }
 }
