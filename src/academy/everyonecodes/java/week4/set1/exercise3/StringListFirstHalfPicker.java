2d4662ae76b960156fea5501988ca6dc28ae19c8
uanave <uanavasile@gmail.com>
2020-01-13 20:38:44 +0100

    added gitignore, added exercise3 with some modifs

--- src/academy/everyonecodes/java/week4/set1/exercise3/StringListFirstHalfPicker.java
+++ src/academy/everyonecodes/java/week4/set1/exercise3/StringListFirstHalfPicker.java
@@ -1,13 +1,19 @@
 package academy.everyonecodes.java.week4.set1.exercise3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class StringListFirstHalfPicker {
     public String pick(List<String> randomWords) {
+        List<String> randomWordsFirstHalf = new ArrayList<>();
+        int size = randomWords.size();
+        double start = Math.floor((double) size/2);
+        for (int i = (int) start; i < size; i++) {
+            randomWordsFirstHalf.add(randomWords.get(i));
+        }
         Random random = new Random();
-        int size = randomWords.size()/2;
-        int randomIndex = random.nextInt(size);
-        return randomWords.get(randomIndex);
+        int index = random.nextInt(randomWordsFirstHalf.size());
+        return randomWordsFirstHalf.get(index);
     }
 }
