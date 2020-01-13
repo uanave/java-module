7bc3b8cac7b257da0c4135bf474f01ea7183afa0
uanave <uanavasile@gmail.com>
2020-01-13 09:53:48 +0100

    added reflection modified and gitignore

--- src/academy/everyonecodes/java/week3/reflection/exercise1/DoubleListSumCalculatorTest.java
+++ src/academy/everyonecodes/java/week3/reflection/exercise1/DoubleListSumCalculatorTest.java
@@ -14,16 +14,16 @@ import java.util.stream.Stream;
 public class DoubleListSumCalculatorTest {
     DoubleListSumCalculator doubleListSumCalculator = new DoubleListSumCalculator();
 
-    private static Stream<Arguments> generateArgumentsStream() {
-        List<Arguments> listOfArguments = new LinkedList<>();
-        listOfArguments.add(Arguments.of(generateArgumentsStream(4, (1.5, 2.5))));
-        listOfArguments.add(Arguments.of(generateArgumentsStream(7.5, (1.5, 2.5, 3.5))));
-        return listOfArguments.stream();
+    private static Stream<Arguments> provideLists() {
+        return Stream.of(
+                Arguments.of(4.0, List.of(1.5, 2.5)),
+                Arguments.of(7.5, List.of(1.5, 2.5, 3.5)),
+                Arguments.of(1.0, List.of(1.0))
+        );
     }
 
-
     @ParameterizedTest
-    @MethodSource("generateArgumentStream")
+    @MethodSource("provideLists")
     void calculate(double expected, List<Double> numbers) {
         double result = doubleListSumCalculator.calculate(numbers);
 
