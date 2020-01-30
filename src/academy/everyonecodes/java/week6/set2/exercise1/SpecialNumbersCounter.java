d788edf8516ad03102406fb0bb9b30ce2ea5f993
uanave <uanavasile@gmail.com>
2020-01-30 20:22:17 +0100

    minor

--- src/academy/everyonecodes/java/week6/set2/exercise1/SpecialNumbersCounter.java
+++ src/academy/everyonecodes/java/week6/set2/exercise1/SpecialNumbersCounter.java
@@ -3,11 +3,13 @@ package academy.everyonecodes.java.week6.set2.exercise1;
 import java.util.List;
 
 public class SpecialNumbersCounter {
+
+    private List<String> numbers = List.of("3", "6", "9");
+
     public int count(int number) {
         int count = 0;
         String numberToString = String.valueOf(number);
         List<String> inputToList = List.of(numberToString.split(""));
-        List<String> numbers = List.of("3", "6", "9");
         for (String nr : inputToList) {
             if (numbers.contains(nr)) {
                 count++;
