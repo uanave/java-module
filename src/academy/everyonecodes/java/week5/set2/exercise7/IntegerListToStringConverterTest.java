fccd063dd6c86419bb11162b26a45417efa3033d
uanave <uanavasile@gmail.com>
2020-02-02 18:08:34 +0100

    minor change week5

--- src/academy/everyonecodes/java/week5/set2/exercise7/IntegerListToStringConverterTest.java
+++ src/academy/everyonecodes/java/week5/set2/exercise7/IntegerListToStringConverterTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class IntegerToListConverterTest {
+public class IntegerListToStringConverterTest {
     IntegerListToStringConverter integerListToStringConverter = new IntegerListToStringConverter();
 
     @Test
