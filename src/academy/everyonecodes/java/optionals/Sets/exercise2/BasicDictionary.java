27921ee04927bfd3cabe9b22c2dbb3ae5ebdeddc
uanave <uanavasile@gmail.com>
2020-02-13 19:01:32 +0100

    optionals

--- src/academy/everyonecodes/java/optionals/Sets/exercise2/BasicDictionary.java
+++ src/academy/everyonecodes/java/optionals/Sets/exercise2/BasicDictionary.java
@@ -1,20 +1,21 @@
 package academy.everyonecodes.java.optionals.Sets.exercise2;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BasicDictionary {
     Set<String> dictionary = Set.of("hi", "house", "dog", "cat", "table", "chair", "elephant");
 
-    public List<String> findUnknownWords(String words) {
-        Set<String> wordsAsSet = new HashSet<>(Arrays.asList(words.split(" ")));
-        return wordsAsSet.stream()
-                .map(String::toLowerCase)
-                .filter(word -> !dictionary.contains(word))
-                .collect(Collectors.toList());
-    }
 
+    public List<String> findUnknownWords(String words) {
+        words = words.toLowerCase();
+        Set<String> wordsAsSet = new LinkedHashSet<>(Arrays.asList(words.split(" ")));
+        if (!words.isBlank()) {
+            return wordsAsSet.stream()
+                    .filter(word -> !dictionary.contains(word))
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<>();
+        }
+    }
 }
--- src/academy/everyonecodes/java/optionals/Sets/exercise2/BasicDictionaryTest.java
+++ src/academy/everyonecodes/java/optionals/Sets/exercise2/BasicDictionaryTest.java
@@ -1,11 +1,11 @@
 package academy.everyonecodes.java.optionals.Sets.exercise2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Stream;
 
 class BasicDictionaryTest {
@@ -17,7 +17,6 @@ class BasicDictionaryTest {
                 Arguments.of(List.of(), ""),
                 Arguments.of(List.of(), "Hi house dog cat table chair elephant"),
                 Arguments.of(List.of("the"), "Hi the house dog cat table chair elephant")
-
         );
     }
 
@@ -25,5 +24,6 @@ class BasicDictionaryTest {
     @MethodSource("arguments")
     void test(List<String> expected, String input) {
         List<String> result = basicDictionary.findUnknownWords(input);
+        Assertions.assertEquals(expected, result);
     }
 }
