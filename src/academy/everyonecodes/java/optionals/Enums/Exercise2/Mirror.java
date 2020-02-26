788f52ca0fa038417a2d369377dc1e5576779ccd
uanave <uanavasile@gmail.com>
2020-02-26 12:34:45 +0100

    enums small change

--- src/academy/everyonecodes/java/optionals/Enums/Exercise2/Mirror.java
+++ src/academy/everyonecodes/java/optionals/Enums/Exercise2/Mirror.java
@@ -4,7 +4,7 @@ import java.time.DayOfWeek;
 import java.time.Month;
 
 public class Mirror {
-    public void sayHowItLooks(DayOfWeek day, Month month, Outfit outfit) {
-        System.out.println("On a " + day.name() + " in " + month.name() + " Hansel is wearing " + outfit.getDescription() +".");
+    public String sayHowItLooks(DayOfWeek day, Month month, Outfit outfit) {
+        return "On a " + day.name() + " in " + month.name() + " Hansel is wearing " + outfit.getDescription() +".";
     }
 }
